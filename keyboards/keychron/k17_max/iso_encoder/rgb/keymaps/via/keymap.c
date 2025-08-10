aaa564eebb55e2551f90926490e4827f61fcd79f
KeithM999 <keithm999@gmail.com>
2025-08-10 13:29:21 +0100

    Messing about with layers

--- keyboards/keychron/k17_max/iso_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k17_max/iso_encoder/rgb/keymaps/via/keymap.c
@@ -25,6 +25,9 @@
 // Custom keycodes
 enum keyboard_keycodes {
     TL_NAV = SAFE_RANGE,
+    TL_MACRO,
+    EMAIL_PE,
+    EMAIL_WO,
 };
 
 enum layers{
@@ -52,16 +55,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,       KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                 KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,        KC_HOME,  KC_P4,    KC_P5,    KC_P6,
-        OSM(MOD_LSFT),  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              OSM(MOD_RSFT), KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
-        KC_LGUI,        KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  KC_RGUI,  OSL(MAC_FN), KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT         ),
+        OSM(MOD_LSFT),  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             OSM(MOD_RSFT), KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+        KC_LGUI,        KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  KC_RGUI, TT(MAC_FN), KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT         ),
 
     [MAC_FN] = LAYOUT_105_iso(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  RGB_TOG,  _______,  _______,  _______,  _______,  RGB_TOG,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  RGB_TOG,  TG(MACRO),  _______,  _______,  _______,  RGB_TOG,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,  _______,  _______,  _______,  _______,
-        TL_NAV,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,                      _______,  _______,  _______,  _______,  _______,
-        RESET,    RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            KC_END,   _______,  _______,  _______,
+        TG(NAVIGATION),  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,    _______,    _______,                      _______,  _______,  _______,  _______,  _______,
+        RESET,    RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,            KC_END,   _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  RESET,      _______,  _______,  _______,  _______,  _______,            _______         ),
+        _______,  _______,  _______,                                _______,                                _______,  _______, RESET,        _______,  _______,  _______,  _______,            _______         ),
 
     [WIN_BASE] = LAYOUT_105_iso(
         KC_ESC,        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,    KC_PSCR, RGB_MOD,  KC_DEL,   _______,  _______,  _______,  KC_MUTE,
@@ -82,18 +85,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MACRO] = LAYOUT_105_iso(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RESET  ,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  EMAIL_WO,  EMAIL_PE,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,
         RESET  ,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  RESET  ,  _______,  _______,  _______,  _______,  _______,            _______         ),
+        _______,  _______,  _______,                                _______,                                _______,  _______, RESET,   _______,  _______,  _______,  _______,            _______         ),
 
     [NAVIGATION] = LAYOUT_105_iso(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,
-        RESET  ,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,
-        RESET  ,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,            _______,  _______,  _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  RESET  ,  _______,  _______,  _______,  _______,  _______,            _______         ),
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,
+        RESET  ,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END ,   _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,
+        RESET  ,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP  ,  KC_RIGHT,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,  _______,            _______,  _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                 _______,  _______,  RESET,  _______,  _______,  _______,  _______,            _______         ),
 
 };
 
@@ -112,16 +115,33 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_ENTER:
-      // Play a tone when enter is pressed
       if (record->event.pressed) {
-        // PLAY_SONG(tone_qwerty);
+      }
+      return true; // Let QMK send the enter press/release events
+
+    case EMAIL_PE:
+      if (record->event.pressed) {
+        SEND_STRING("Personal email");
+      }
+      return true; // Let QMK send the enter press/release events
+
+    case EMAIL_WO:
+      if (record->event.pressed) {
+        SEND_STRING("Work Email");
+      }
+      return true; // Let QMK send the enter press/release events
+
+    case TL_MACRO:
+      if (record->event.pressed) {
+          layer_on(MACRO);
+          SEND_STRING("Toggle Macro...");
       }
       return true; // Let QMK send the enter press/release events
 
     case TL_NAV:
-      // Play a tone when enter is pressed
       if (record->event.pressed) {
-        SEND_STRING("TL-NAV key pressed");
+          layer_on(NAVIGATION);
+          SEND_STRING("Toggle Navigation...");
       }
       return true; // Let QMK send the enter press/release events
 
