cbc1ffd8447dda0715c965c65e3d1e2b2933e586
KeithM999 <keithm999@gmail.com>
2025-08-10 14:01:04 +0100

    Set Tab tap to keypress, Tab hold to Navigation layer

--- keyboards/keychron/k17_max/iso_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k17_max/iso_encoder/rgb/keymaps/via/keymap.c
@@ -19,6 +19,8 @@
 
 // Definitions - stick to 8 chars or less to help with layout in the keymaps
 #define RESET TO(MAC_BASE)
+#define TAB_TAP_HOLD LT(NAVIGATION, KC_TAB)
+
 #define FN_CAPS LT(_FL, KC_CAPS)
 #define ALT_TAB LALT(KC_TAB)
 
@@ -53,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_105_iso(
         KC_ESC,         KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_SNAP,       RGB_MOD,  KC_DEL,   KC_F13,   KC_F14,   KC_F15,   KC_MUTE,
         KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,       KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                 KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+        TAB_TAP_HOLD,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                 KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_ESC,         KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,        KC_HOME,  KC_P4,    KC_P5,    KC_P6,
         OSM(MOD_LSFT),  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             OSM(MOD_RSFT), KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LGUI,        KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  KC_RGUI, TT(MAC_FN), KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT         ),
